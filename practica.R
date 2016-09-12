eada5a65d575096a993b9df4bd68b90cb79bae51
Francisco Garcia <paco_garcia96@hotmail.com>
2016-09-12 10:51:13 -0500

    practica
    experimento

---
+++ practica.R
@@ -0,0 +1,23 @@
+varu <- 0 
+for (i in 1:100) {
+  z<-5  
+  while (z>=3 && z<=10){
+    #print (z) 
+    moneda<- rbinom(1,1,0.5)
+    
+    if (moneda==1){
+      z<- z+1
+      
+    } else {
+      z<-z-1
+      
+    }
+  }
+  print(z)
+  if(z<3){ varu <- varu +1} 
+}
+
+
+varu
+vart<-100-varu
+vart
