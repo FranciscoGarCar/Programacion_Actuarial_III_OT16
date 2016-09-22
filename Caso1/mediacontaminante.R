73073397da5142c5653d13b073d13ea303a17100
Francisco Garcia <paco_garcia96@hotmail.com>
2016-09-22 04:11:22 -0500

    caso1

--- Caso1/mediacontaminante.R
+++ Caso1/mediacontaminante.R
@@ -1,23 +1,70 @@
-varu <- 0 
-for (i in 1:100) {print(z)
-z<-5  
-while (z>=3 && z<=10){
-  #print (z) 
-  moneda<- rbinom(1,1,0.5)
-  
-  if (moneda==1){
-    z<- z+1
-    
-  } else {
-    z<-z-1
-    
+mediacontaminante<- function(directorio="~/GitHub/Programacion_Actuarial_III_OT16/specdata/specdata",contaminante,id=1:332){
+  getwd()
+  setwd("~/GitHub/Programacion_Actuarial_III_OT16/specdata/specdata")
+  promedio<-0
+  for (i in id){
+    if (contaminante=="sulfato"){
+      if(i<10){
+        c<- paste("00",i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,2],na.rm=T))
+        
+      }
+      if(i==10){
+        c<- paste("0",i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,2],na.rm=T))
+      }
+      if(i>10 && i<100){
+        c<- paste("0",i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,2],na.rm=T))
+      }
+      if(i==100){
+        c<- paste(i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,2],na.rm=T))
+      }
+      if(i>100){
+        c<- paste(i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,2],na.rm=T))
+      }
+      promedio1<-promedio/length(id)
+    }
+    else {
+      if(i<10){
+        c<- paste("00",i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,3],na.rm=T))
+      }
+      if(i==10){
+        c<- paste("0",i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,3],na.rm=T))
+      }
+      if(i>10 && i<100){
+        c<- paste("0",i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,3],na.rm=T))
+      }
+      if(i==100){
+        c<- paste(i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,3]-d[1,3],na.rm=T))
+      }
+      if(i>100){
+        c<- paste(i,".csv",sep="")
+        d<-read.csv(c)
+        promedio<- promedio+(mean(d[,3],na.rm=T))
+      }
+     
+    }
+   
   }
+  promedio1<-promedio/length(id) 
+  promedio1
+ 
 }
-if(z<3){ varu <- varu +1} 
-}
 
-
-varu
-vart<-100-varu
-vart
-
+mediacontaminante(contaminante="sulfato",1:1)
